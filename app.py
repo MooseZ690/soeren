b8af786a3007fab5fc02f03af951cb5ceca3d1bf
MooseZ690 <23248@burnside.school.nz>
2026-07-26 09:29:49 +1200

    new banner + admin page setup

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, redirect, url_for, session, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "p943j8ow7kw9p05e7qkw0"
 
 DATABASE = "database.db"
 
@@ -39,10 +41,43 @@ def execute_db(query, args=()):
 
 @app.route("/")
 def home():
+    session.clear()
     cc="SELECT * FROM character;"
+    posts="SELECT * FROM posts ORDER BY time ASC;"
     cc=query_db(cc)
-    return render_template("home.html", cc=cc)
+    posts=query_db(posts)
+    return render_template("home.html", cc=cc, posts=posts)
 
+@app.route("/contact")
+def contact():
+    session.clear()
+    return render_template("contact.html")
+
+@app.route("/admin")
+def admin():
+    if not session.get("logged_in"):
+        return redirect(url_for("login"))
+
+    return render_template("admin.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = query_db(
+            "SELECT * FROM admin WHERE username = ?",
+            (username,),
+            one=True
+        )
+
+        if user and check_password_hash(user["password"], password):
+            session["logged_in"] = True
+            session["username"] = username
+            return redirect(url_for("admin"))
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
